e0990c54de758d3c9ca18e656f04e30de31a19ba
javaone <richardsang2008@gmail.com>
2016-07-27 13:51:15 -0700

    first p4 check in

--- cloudformation/create_vpc.py
+++ cloudformation/create_vpc.py
@@ -1,8 +1,12 @@
 import os
 
-from troposphere import Template, Tags, Parameter, Join, Ref, Output
+from troposphere import Template, Tags, Parameter, Join, Ref, Output, GetAZs,FindInMap,Base64
 from troposphere.ec2 import VPC, InternetGateway, RouteTable, NetworkAcl, Route, Subnet, VPNGateway, SubnetNetworkAclAssociation,SubnetRouteTableAssociation, \
-    NetworkAclEntry, PortRange
+    NetworkAclEntry, PortRange, VPCGatewayAttachment
+
+import troposphere.ec2 as ec2
+
+import troposphere.elasticloadbalancing as elb
 
 # varible can be refactored later
 vpcenvcidr = 16
@@ -15,6 +19,15 @@ t = Template()
 t.add_description("GD Cloud Infrastructure")
 env = t.add_parameter(Parameter("Environment", Type="String", Description="Demo deplyment environment", Default="dev"))
 vpc = None
+privateSubnet1 = None
+privateSubnet2 = None
+privateSubnet3 = None
+privateSubnet4 = None
+privateSubnet5 = None
+privateSubnet6 = None
+publicSubnet1 = None
+publicSubnet2 = None
+internetGateway = None
 
 
 def generate_vpc_template():
@@ -32,6 +45,19 @@ def generate_vpc_template():
 
 
 def generate_netowrk_template():
+    global  internetGateway
+    internetGateway = t.add_resource(InternetGateway(
+        "InternetGateway",
+        Tags=Tags(
+            Name=Join("", [ Ref(env), "_IneterNetGateway",]),
+            Application=ref_stack_id,
+        )
+    ))
+    gatewayAttachment = t.add_resource(VPCGatewayAttachment(
+        "AttachGateway",
+        VpcId=Ref(vpc),
+        InternetGatewayId=Ref("InternetGateway")
+    ))
 
     publicRouteTable = t.add_resource(RouteTable(
         "PublicRouteTable",
@@ -63,20 +89,15 @@ def generate_netowrk_template():
     ))
 
 
-    vpnGateway = t.add_resource(VPNGateway(
-        "VPNGateway",
-        Type="ipsec.1",
-        Tags=Tags(
-            Application=Ref(env),
-        )
-    ))
+
     route = t.add_resource(Route(
         "Route",
-        GatewayId=Ref(vpnGateway),
+        GatewayId=Ref(internetGateway),
         DestinationCidrBlock="0.0.0.0/0",
         RouteTableId=Ref(privateRouteTable),
     ))
    #define 2 public public subnet
+    global  publicSubnet1
     publicSubnet1 = t.add_resource(Subnet(
         "PublicSubnet1",
         VpcId=Ref(vpc),
@@ -86,7 +107,7 @@ def generate_netowrk_template():
             Network= Join("", [Ref(env), "_Public Subnet 1"]),
         )
     ))
-
+    global publicSubnet2
     publicSubnet2 = t.add_resource(Subnet(
         "PublicSubnet2",
         VpcId=Ref(vpc),
@@ -100,6 +121,7 @@ def generate_netowrk_template():
     # publicSubnets[0]= publicSubnet1
     # publicSubnets[1] = publicSubnet2
     #define 6 private subnet
+    global privateSubnet1
     privateSubnet1 = t.add_resource(Subnet(
         "PrivateSubnet1",
         VpcId=Ref(vpc),
@@ -109,7 +131,7 @@ def generate_netowrk_template():
             Network=Join("", [Ref(env), "_Private Subnet1"])
         )
     ))
-
+    global privateSubnet2
     privateSubnet2 = t.add_resource(Subnet(
         "PrivateSubnet2",
         VpcId=Ref(vpc),
@@ -119,6 +141,7 @@ def generate_netowrk_template():
             Network=Join("", [Ref(env), "_Private Subnet 2"])
         )
     ))
+    global privateSubnet3
     privateSubnet3 = t.add_resource(Subnet(
         "PrivateSubnet3",
         VpcId=Ref(vpc),
@@ -128,6 +151,7 @@ def generate_netowrk_template():
             Network=Join("", [Ref(env), "_Private Subnet 3"])
         )
     ))
+    global privateSubnet4
     privateSubnet4 = t.add_resource(Subnet(
         "PrivateSubnet4",
         VpcId=Ref(vpc),
@@ -137,6 +161,7 @@ def generate_netowrk_template():
             Network=Join("", [Ref(env), "_Private Subnet 4"])
         )
     ))
+    global privateSubnet5
     privateSubnet5 = t.add_resource(Subnet(
         "PrivateSubnet5",
         VpcId=Ref(vpc),
@@ -146,6 +171,7 @@ def generate_netowrk_template():
             Network=Join("", [Ref(env), "_Private Subnet 5"])
         )
     ))
+    global privateSubnet6
     privateSubnet6 = t.add_resource(Subnet(
         "PrivateSubnet6",
         VpcId=Ref(vpc),
@@ -243,33 +269,102 @@ def generate_netowrk_template():
         RuleAction="allow",
         CidrBlock="0.0.0.0/0",
     ))
-    # outputs = t.add_output(Output(
-    #     "VPCId",
-    #     Description="VPCId of the newly created VPC",
-    #     Value=Ref(VPC),
-    # ))
 
 
-outputs = t.add_output([
-    Output('VPC', Value=Ref(vpc)),
-    # Output('IGW', Value=Ref(internetGateway)),
-    Output('PublicSubnet1', Value=Ref("PublicSubnet1")),
-    Output('PublicSubnet2', Value=Ref("PublicSubnet2")),
-    Output('PrivateSubnet1', Value=Ref("PrivateSubnet1")),
-    Output('PrivateSubnet2', Value=Ref("PrivateSubnet2")),
-    Output('PrivateSubnet3', Value=Ref("PrivateSubnet3")),
-    Output('PrivateSubnet4', Value=Ref("PrivateSubnet4")),
-    Output('PrivateSubnet5', Value=Ref("PrivateSubnet5")),
-    Output('PrivateSubnet6', Value=Ref("PrivateSubnet6")),
+def generate_ec2_template():
+    # Add the Parameters
+    keyname_param = t.add_parameter(Parameter(
+        "KeyName",
+        Type="String",
+        Default="mark",
+        Description="Name of an existing EC2 KeyPair to "
+                    "enable SSH access to the instance",
+    ))
 
-    # Output('SGlocal', Value=Ref(sglocal))
-])
+    t.add_parameter(Parameter(
+        "InstanceType",
+        Type="String",
+        Description="WebServer EC2 instance type",
+        Default="m1.small",
+        AllowedValues=[
+            "t1.micro", "m1.small", "m1.medium", "m1.large", "m1.xlarge",
+            "m2.xlarge", "m2.2xlarge", "m2.4xlarge", "c1.medium", "c1.xlarge",
+            "cc1.4xlarge", "cc2.8xlarge", "cg1.4xlarge"
+        ],
+        ConstraintDescription="must be a valid EC2 instance type.",
+    ))
 
-template = (t.to_json())
+    webport_param = t.add_parameter(Parameter(
+        "WebServerPort",
+        Type="String",
+        Default="8888",
+        Description="TCP/IP port of the web server",
+    ))
+
+    # Define the instance security group
+    instance_sg = t.add_resource(
+        ec2.SecurityGroup(
+            "InstanceSecurityGroup",
+            GroupDescription="Enable SSH and HTTP access on the inbound port",
+            SecurityGroupIngress=[
+                ec2.SecurityGroupRule(
+                    IpProtocol="tcp",
+                    FromPort="22",
+                    ToPort="22",
+                    CidrIp="0.0.0.0/0",
+                ),
+                ec2.SecurityGroupRule(
+                    IpProtocol="tcp",
+                    FromPort=Ref(webport_param),
+                    ToPort=Ref(webport_param),
+                    CidrIp="0.0.0.0/0",
+                ),
+            ]
+        )
+    )
+
+    # Add the web server instances
+    web_instances = []
+    for name in ("Ec2Instance1", "Ec2Instance2"):
+        instance = t.add_resource(ec2.Instance(
+            name,
+            SecurityGroups=[Ref(instance_sg)],
+            KeyName=Ref(keyname_param),
+            InstanceType=Ref("InstanceType"),
+            ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
+            UserData=Base64(Ref(webport_param)),
+        ))
+        web_instances.append(instance)
+
+    elasticLB = t.add_resource(elb.LoadBalancer(
+        'ElasticLoadBalancer',
+        AvailabilityZones=GetAZs(""),
+        ConnectionDrainingPolicy=elb.ConnectionDrainingPolicy(
+            Enabled=True,
+            Timeout=300,
+        ),
+        Instances=[Ref(r) for r in web_instances],
+        Listeners=[
+            elb.Listener(
+                LoadBalancerPort="80",
+                InstancePort=Ref(webport_param),
+                Protocol="HTTP",
+            ),
+        ],
+        HealthCheck=elb.HealthCheck(
+            Target=Join("", ["HTTP:", Ref(webport_param), "/"]),
+            HealthyThreshold="3",
+            UnhealthyThreshold="5",
+            Interval="30",
+            Timeout="5",
+        )
+    ))
+
+
 if __name__ == '__main__':
     generate_vpc_template()
     generate_netowrk_template()
-
+    generate_ec2_template()
     template = t.to_json()
     directory = "vpc_template"
     if not os.path.exists(directory):
